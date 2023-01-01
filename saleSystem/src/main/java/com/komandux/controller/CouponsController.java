ce9df7e36c7a8382381f4d299e9b8a75533b66c6
EligijusSalk <90383690+EligijusSalk@users.noreply.github.com>
2023-01-01 22:36:39 +0200

    Coupons Controller

---
+++ saleSystem/src/main/java/com/komandux/controller/CouponsController.java
@@ -0,0 +1,106 @@
+package com.komandux.controller;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.komandux.Tables;
+import com.komandux.model.Coupon;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+public class CouponsController {
+
+	@ApiModel
+	private static class CouponsDTO {
+
+		@ApiModelProperty(dataType = "int")
+		public int organization_id;
+		@ApiModelProperty(dataType = "JSONObject")
+		public Coupon coupon;
+		public CouponsDTO(int organization_id, Coupon coupon) {
+			super();
+			this.organization_id = organization_id;
+			this.coupon = coupon;
+		}
+    }
+
+	@ApiModel
+	private static class CouponsResponseDTO extends CouponsDTO {
+
+		public CouponsResponseDTO(int organization_id, Coupon coupon) {
+			super(organization_id, coupon);
+		}
+	}
+
+    // Add coupon
+	@ApiOperation(value = "Add Coupon", response = Coupon.class, tags = "Coupons")
+	@PostMapping(value = "/organizations/{organization_id}/coupons")
+	public ResponseEntity<?> createCoupon(@PathVariable(value = "organization_id") int organization_id,
+		@RequestBody Coupon coupon) {
+
+		String sql = "INSERT INTO coupons (cust_id, code, created_timestamp, valid_until) VALUES ("
+		+ coupon.getCust_id() + ",'" + coupon.getCode() + "','" + coupon.getCreated_timestamp() + "','" + coupon.getValid_unit() + "');";
+
+		System.out.println(sql);
+
+		try {
+			Connection connection = DriverManager.getConnection(Tables.getJdbcUrl());
+			Statement statement;
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+
+			connection.close();
+			statement.close();
+			return new ResponseEntity<CouponsResponseDTO>(
+					new CouponsResponseDTO(organization_id, coupon), HttpStatus.OK);
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+			return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+	}
+
+	// putEmployee endpoint
+	@ApiOperation(value = "Update Coupon", response = Coupon.class, tags = "Coupons")
+	@PutMapping(value = "/organizations/{organization_id}/coupons{coupon_id}")
+	public ResponseEntity<?> putEmployee(@PathVariable(value = "organization_id") int organization_id,
+			@PathVariable(value = "coupon_id") int coupon_id, @RequestBody Coupon coupon)
+			throws ParseException {
+
+		String sql = "UPDATE coupons SET " + "cust_id = \'" + coupon.getCust_id() + "'," + "code ='"
+				+ coupon.getCode() + "'," + "created_timestamp ='" + coupon.getCreated_timestamp() + "'," + "valid_until ='"
+				+ coupon.getValid_unit() + "' " + "WHERE id= " + coupon_id + ";";
+
+		try {
+			Connection connection = DriverManager.getConnection(Tables.getJdbcUrl());
+			Statement statement;
+
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+
+			statement.close();
+
+			return new ResponseEntity<CouponsResponseDTO>(
+					new CouponsResponseDTO(organization_id, coupon), HttpStatus.OK);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+	}
+
+
+}
--- saleSystem/src/main/java/com/komandux/model/Coupon.java
+++ saleSystem/src/main/java/com/komandux/model/Coupon.java
@@ -1,29 +1,31 @@
 package com.komandux.model;
 
+import java.security.Timestamp;
+import java.util.Date;
+import java.sql.Time;
 import java.time.LocalTime;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel
-public class Coupon extends Discount {
+public class Coupon {
 	@ApiModelProperty(dataType="int")
 	private int coupon_id;
 	@ApiModelProperty(dataType="int")
 	private int cust_id;
 	@ApiModelProperty(dataType="int")
-	private int discount_id;
-	@ApiModelProperty(dataType="int")
 	private int code;
-	@ApiModelProperty(dataType="LocalTime")
-	private LocalTime valid_unit;
+	@ApiModelProperty(dataType="Date")
+	private Date valid_unit;
+	@ApiModelProperty(dataType="Date")
+	private Date created_timestamp;
 	
-	public Coupon(int coupon_id, int cust_id, int discount_id, int code, LocalTime valid_unit, int id, int service_id, int percentage_off, int exact_price, LocalTime created_timestamp) {
-		super(id, service_id, percentage_off, exact_price, created_timestamp);
+	public Coupon(int coupon_id, int cust_id, int code, Date created_timestamp, Date valid_unit) {
 		this.coupon_id = coupon_id;
 		this.cust_id = cust_id;
-		this.discount_id = discount_id;
 		this.code = code;
+		this.created_timestamp = created_timestamp;
 		this.valid_unit = valid_unit;
 	}
 
@@ -43,13 +45,6 @@ public class Coupon extends Discount {
 		this.cust_id = cust_id;
 	}
 
-	public int getDiscount_id() {
-		return discount_id;
-	}
-
-	public void setDiscount_id(int discount_id) {
-		this.discount_id = discount_id;
-	}
 
 	public int getCode() {
 		return code;
@@ -59,12 +54,20 @@ public class Coupon extends Discount {
 		this.code = code;
 	}
 
-	public LocalTime getValid_unit() {
+	public Date getValid_unit() {
 		return valid_unit;
 	}
 
-	public void setValid_unit(LocalTime valid_unit) {
+	public void setValid_unit(Date valid_unit) {
 		this.valid_unit = valid_unit;
+	}
+
+	public Date getCreated_timestamp() {
+		return created_timestamp;
+	}
+
+	public void setCreated_timestamp(Date created_timestamp) {
+		this.created_timestamp = created_timestamp;
 	}
 	
 }
